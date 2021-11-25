b33e885551c11a5bd929cb789a473c563254d2db
heyimmatt <mrmcfarland@gmail.com>
2021-11-25 12:45:31 -0800

    Configure sequelize, add sync script

---
+++ db/config/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	development: {
+		username: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_DEV_NAME,
+		host: process.env.DB_HOST,
+		dialect: 'postgres',
+	},
+	test: {
+		username: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_TEST_NAME,
+		host: process.env.DB_HOST,
+		dialect: 'postgres',
+	},
+};
