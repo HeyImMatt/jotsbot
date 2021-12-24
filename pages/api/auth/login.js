98d89a251414b567bd10eeb790c580cf6b29c4ff
heyimmatt <mrmcfarland@gmail.com>
2021-12-24 09:35:41 -0700

    Add /auth/login route with POST handler for user login

---
+++ pages/api/auth/login.js
@@ -0,0 +1,27 @@
+import prisma from '../../../lib/prisma';
+import { verifyPasswordHash } from '../../../utils/user';
+
+export default async function handler(req, res) {
+	if (req.method === 'POST') {
+		const { email, password: plainPw } = req.body;
+
+		if (!email || !plainPw) {
+			return res.status(400).send('Email or password cannot be blank.');
+		}
+
+		const user = await prisma.user.findUnique({
+			where: { email },
+			include: { jots: true },
+		});
+
+		const pwMatch = await verifyPasswordHash(plainPw, user.password);
+
+		if (pwMatch) {
+			return res.status(200).json(user);
+		}
+
+		return res.status(401).send('Authentication failed.');
+	}
+
+	return res.status(400).send('Invalid request method.');
+}
